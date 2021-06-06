e26f3b240d2be6c16dfc2d711216db44805c329e
harsha-vardhan-v <v.harshavardhan93@gmail.com>
2021-06-06 23:03:40 +0530

    Added user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+var userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        minLength: 4,
+        maxLength: 20,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    phone: {
+        type: Number,
+        unique: true
+    }
+}, {
+    timestamps: true
+})
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.password
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
