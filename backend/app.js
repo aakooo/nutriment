d6a55b528617d1fb5e9836f9dff221a8fd5c9de4
harsha-vardhan-v <v.harshavardhan93@gmail.com>
2021-06-06 22:28:26 +0530

    Added middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,35 @@
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('express-async-errors')
+
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
 const app = express()
+
+//Connecting to mongoDB
+logger.info('Connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => {
+        logger.info('Connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('Error connecting to MongoDB:', error.message)
+    })
+
+//Middlewares
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.get('/', (request, response) => {
     response.send('<p>Oversee backend api</p>')
 })
+
+app.use(middleware.unknownEndPoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
