a328ecb0bfb7501f0e3e6498d5cdad0d0c08c3d0
Guhan M <60909829+guhan29@users.noreply.github.com>
2021-06-18 17:12:15 +0530

    added milestone route

--- backend/routes/team.route.js
+++ backend/routes/team.route.js
@@ -3,6 +3,7 @@ const teamRouter = require('express').Router()
 const mongoose = require('mongoose')
 const Team = require('../models/team.model')
 const User = require('../models/user.model')
+const Milestone = require('../models/milestone.model')
 
 const jwt = require('jsonwebtoken')
 
@@ -40,6 +41,68 @@ teamRouter.post('/', async (req, res) => {
     await updatedUser.save()
 
     res.json(savedTeam)
+})
+
+// Milestone
+teamRouter.post('/milestone', async (req, res) => {
+    const token = getTokenFrom(req)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    const team = await Team
+            .findById(req.body.team)
+            .populate('admin')
+
+    if (!token || !decodedToken || decodedToken.id != team.admin._id) {
+        return res.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const newMilestone = new Milestone({
+        name: req.body.name,
+        deadline: req.body.deadline,
+        status: req.body.status,
+        team: mongoose.Types.ObjectId(req.body.team)
+    })
+    const savedMilestone = await newMilestone.save()
+
+    res.json(savedMilestone)
+})
+
+teamRouter.get('/milestone', async (req, res) => {
+    const milestones = await Milestone
+        .find({})
+        .populate('team')
+    
+    res.json(milestones.map(t => t.toJSON()))
+})
+
+teamRouter.get('/milestone/:id', async (req, res) => {
+    const milestone = await Milestone
+        .findById(req.params.id)
+        .populate('team')
+    
+    res.json(milestone)
+})
+
+
+teamRouter.delete('/milestone/:id', async (req, res) => {
+    const token = getTokenFrom(req)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    const milestone = await Milestone
+            .findById(req.params.id)
+            .populate({
+                path: 'team',
+                model: 'Team',
+                populate: {
+                    path: 'admin',
+                    model: 'User'
+                }
+            })
+
+    if (!token || !decodedToken || decodedToken.id != milestone.team.admin._id) {
+        return res.status(401).json({ error: 'token missing or invalid' })
+    }
+    const deletedMilestone = await Milestone
+        .findByIdAndDelete(req.params.id)
+    res.json(deletedMilestone)
 })
 
 module.exports = teamRouter
